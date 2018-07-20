7b6458bb3c2e97217935e06c12119f494184dc2e
Rishabh Chauahn <rishabhc32@gmail.com>
2018-07-21 01:13:58 +0530

    rewrote date time handling function using moment library

--- helpers/getTime.js
+++ helpers/getTime.js
@@ -1,16 +1,11 @@
+const moment = require('moment')
+
 function getCurrentTime() {
     return new Promise((resolve, reject) => {
-        let Time = new Date(),
-            hours = Time.getHours() % 12 || 12,
-            ampm = (Time.getHours() < 12 || Time.getHours() === 24) ? "AM" : "PM",
-            minutes = Time.getMinutes();
+        let dateTime = moment()
+        dateTime = dateTime.utcOffset("+05:30").format('DD[/]MM[/]YYYY, h:mm A')
 
-        minutes = minutes < 10 ? '0'+minutes : minutes;
-
-        let time = `${hours}:${Time.getMinutes()} ${ampm} `
-        let date = `${Time.getDate()}/${Time.getMonth()+1}/${Time.getFullYear()}`
-
-        resolve(time + date)
+        resolve(dateTime)
     })
 }
 
