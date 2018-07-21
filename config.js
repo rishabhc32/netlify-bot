721197e00fc02563800eb318ef301e5e371ebcf2
Rishabh Chauahn <rishabhc32@gmail.com>
2018-07-21 18:04:46 +0530

    added telegram entlify bot token

--- config.js
+++ config.js
@@ -5,11 +5,13 @@ module.exports = {
         failureMessage: 'Build Failed'
     },
     telegram: {
-        botToken: process.env.BOT_TOKEN || throwErr(),
+        botToken: process.env.BOT_TOKEN || throwErr('NETLIFY_BUILD_TOKEN'),
         channelUsername: 'netlify_zapcircle'
-    }
+    },
+    triggerBuildMessage: '/start_build',
+    netlifyBuildToken: process.env.NETLIFY_BUILD_TOKEN || throwErr('NETLIFY_BUILD_TOKEN')
 }
 
-function throwErr() {
-    throw new Error('BOT_TOKEN not defined in config.js')
+function throwErr(envVar) {
+    throw new Error(`${envVar} not defined in config.js`)
 }
